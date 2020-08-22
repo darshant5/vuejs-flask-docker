cdae5366ba606356da26cec5e6723e393ecd86be
Darshan Trambadiya <trambadiyadarshan5@gmail.com>
2020-08-22 19:08:08 +0530

    Implement dynamic pressure option with animation & import json file

--- services/frontend/ui/src/components/WheelsSVG.vue
+++ services/frontend/ui/src/components/WheelsSVG.vue
@@ -1,27 +1,218 @@
 <template>
+  <div>
     <div>
-        <svg width="400" height="80" viewBox="0 0 680 100">
-            <polyline points="5,5 100,5 140,60 190,60" style="fill:none;stroke:#c8cccf;stroke-width:4" />
+      <svg width="400" height="80" viewBox="0 0 680 100">
+        <filter
+          id="fillTopRect"
+          primitiveUnits="objectBoundingBox"
+          x="0%"
+          y="0%"
+          width="100%"
+          height="100%"
+        >
+          <feFlood
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            flood-color="#ED8138"
+          />
+          <feOffset :dy="fillTopValue">
+            <animate
+              ref="animateTopRect"
+              attributeName="dy"
+              from="1"
+              :to="fillTopValue"
+              dur="2s"
+            />
+          </feOffset>
+          <feComposite operator="in" in2="SourceGraphic" />
+          <feComposite operator="over" in2="SourceGraphic" />
+        </filter>
 
-            <rect x="200" y="20" rx="15" ry="15" width="50" height="80" style="fill:#FFFFFF;stroke:#DFE3E6;stroke-width:5;opacity:0.8" />
-  
-            <rect x="420" y="20" rx="15" ry="15" width="50" height="80" :style="getRectStyle" />
-  
-            <polyline points="480,60 530,60 570,5 665,5" style="fill:none;stroke:#c8cccf;stroke-width:4" />  
-            Sorry, your browser does not support inline SVG.
-        </svg>
+        <polyline points="5,5 100,5 140,60 190,60" :style="getPolylineStyle" />
+
+        <rect
+          x="200"
+          y="20"
+          rx="15"
+          ry="15"
+          width="50"
+          height="80"
+          :filter="filterTopLeftRect"
+          :style="getRectStyle"
+        />
+
+        <rect
+          x="420"
+          y="20"
+          rx="15"
+          ry="15"
+          width="50"
+          height="80"
+          :filter="filterTopRightRect"
+          :style="getRectStyle"
+        />
+
+        <polyline
+          points="480,60 530,60 570,5 665,5"
+          :style="getPolylineStyle"
+        />
+        Sorry, your browser does not support inline SVG.
+      </svg>
     </div>
+    <div>
+      <svg width="400" height="80" viewBox="0 0 680 100">
+        <filter
+          id="fillBottomRect"
+          primitiveUnits="objectBoundingBox"
+          x="0%"
+          y="0%"
+          width="100%"
+          height="100%"
+        >
+          <feFlood
+            x="0%"
+            y="0%"
+            width="100%"
+            height="100%"
+            flood-color="#ED8138"
+          />
+          <feOffset :dy="fillBottomValue">
+            <animate
+              ref="animateBottomRect"
+              attributeName="dy"
+              from="1"
+              :to="fillBottomValue"
+              dur="2s"
+            />
+          </feOffset>
+          <feComposite operator="in" in2="SourceGraphic" />
+          <feComposite operator="over" in2="SourceGraphic" />
+        </filter>
+
+        <polyline points="5,5 100,5 140,60 190,60" :style="getPolylineStyle" />
+
+        <rect
+          x="200"
+          y="20"
+          rx="15"
+          ry="15"
+          width="50"
+          height="80"
+          :filter="filterBottomLeftRect"
+          :style="getRectStyle"
+        />
+
+        <rect
+          x="420"
+          y="20"
+          rx="15"
+          ry="15"
+          width="50"
+          height="80"
+          :filter="filterBottomRightRect"
+          :style="getRectStyle"
+        />
+
+        <polyline
+          points="480,60 530,60 570,5 665,5"
+          :style="getPolylineStyle"
+        />
+        Sorry, your browser does not support inline SVG.
+      </svg>
+    </div>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "WheelsSVG",
-        props: ['isRightFilled'],
-        computed: {
-            getRectStyle() {
-                const common = 'stroke-width:5;opacity:0.8;';
-                return this.isRightFilled === 'true' ? `${common}fill:#ED8138;stroke:#DA7235;` : `${common}fill:#FFFFFF;stroke:#DFE3E6;`
-            },
-        }
-    }
+export default {
+  name: "WheelsSVG",
+
+  props: ["areaPosition", "selectedPressureOption"],
+
+  data: () => ({
+    filterTopLeftRect: undefined,
+    filterTopRightRect: undefined,
+    filterBottomLeftRect: undefined,
+    filterBottomRightRect: undefined,
+    fillTopValue: undefined,
+    fillBottomValue: undefined,
+  }),
+
+  mounted: function() {
+    this.highlightSVGRect(this.selectedPressureOption);
+  },
+
+  methods: {
+    highlightSVGRect(data) {
+      this.resetFillRectValues();
+
+      const position = this.$_.get(data, "position");
+      const value = Math.abs(this.$_.get(data, "value") / 5 - 1);
+
+      switch (position) {
+        case "top-left":
+          this.filterTopLeftRect = "url(#fillTopRect)";
+          this.fillTopValue = value;
+          break;
+        case "top-right":
+          this.filterTopRightRect = "url(#fillTopRect)";
+          this.fillTopValue = value;
+          break;
+        case "bottom-left":
+          this.filterBottomLeftRect = "url(#fillBottomRect)";
+          this.fillBottomValue = value;
+          break;
+        case "bottom-right":
+          this.filterBottomRightRect = "url(#fillBottomRect)";
+          this.fillBottomValue = value;
+          break;
+        default:
+          break;
+      }
+
+      this.animateRect(position);
+    },
+    resetFillRectValues() {
+      this.filterTopLeftRect = undefined;
+      this.filterTopRightRect = undefined;
+      this.filterBottomLeftRect = undefined;
+      this.filterBottomRightRect = undefined;
+      this.fillTopValue = undefined;
+      this.fillBottomValue = undefined;
+    },
+    animateRect(position) {
+      if (position === "top-left" || position === "top-right") {
+        this.$refs.animateTopRect.beginElement();
+      } else {
+        this.$refs.animateBottomRect.beginElement();
+      }
+    },
+  },
+
+  computed: {
+    getPolylineStyle() {
+      return "fill:none;stroke:#c8cccf;stroke-width:4";
+    },
+    getRectStyle() {
+      return "fill:#FFFFFF;stroke:#DFE3E6;stroke-width:1;opacity:1";
+    },
+  },
+
+  watch: {
+    selectedPressureOption: {
+      handler(data) {
+        this.highlightSVGRect(data);
+      },
+      deep: true,
+    },
+  },
+};
+
+// https://stackoverflow.com/questions/29368138/outlining-and-partially-filling-an-svg-shape
+// http://tutorials.jenkov.com/svg/rect-element.html
+// https://www.w3schools.com/graphics/svg_filters_intro.asp
+// https://yoksel.github.io/svg-filters/#/
+// https://www.tutorialspoint.com/svg/svg_patterns.htm
 </script>
--- services/frontend/ui/src/views/ControlPanel.vue
+++ services/frontend/ui/src/views/ControlPanel.vue
@@ -5,12 +5,17 @@
     </header>
 
     <main>
-      <div class="control-panel__select animate__animated animate__fadeInDown animate__slow animate__delay-0.9s">
+      <div
+        class="control-panel__select animate__animated animate__fadeInDown animate__slow animate__delay-0.9s"
+      >
         <v-container fluid>
           <v-row align="center">
             <v-col class="d-flex">
               <v-select
                 :items="pressureOptions"
+                :return-object="true"
+                item-text="value"
+                v-model="selectedPressureOption"
                 background-color="#D9E5F0"
                 label="Select: Tires Pressure"
                 solo
@@ -22,32 +27,45 @@
         </v-container>
       </div>
 
-      <div class="control-panel__wheels animate__animated animate__fadeInDown animate__slow animate__delay-0.9s">
-        <span class="top-left animate__animated animate__zoomInLeft animate__slow animate__delay-1s">2.2 BAR</span>
-        <span class="top-right animate__animated animate__zoomInRight animate__slow animate__delay-1s">1.9 BAR</span>
-        <span class="bottom-left animate__animated animate__zoomInLeft animate__slow animate__delay-1s">2.2 BAR</span>
-        <span class="bottom-right animate__animated animate__zoomInRight animate__slow animate__delay-1s">2.2 BAR</span>
+      <div
+        class="control-panel__wheels animate__animated animate__fadeInDown animate__slow animate__delay-0.9s"
+      >
+        <span
+          v-for="poption in pressureOptions"
+          :key="poption.value"
+          :class="
+            poption.position +
+              ' animate__animated animate__zoomInLeft animate__slow animate__delay-1s ' +
+              poption.animation
+          "
+        >
+          {{ poption.value }} BAR
+        </span>
 
-        <img src="../assets/wheel-chassis.png" class="chassis animate__animated animate__zoomIn animate__slow animate__delay-2s" />
+        <img
+          src="../assets/wheel-chassis.png"
+          class="chassis animate__animated animate__zoomIn animate__slow animate__delay-2s"
+        />
 
-        <WheelsSVG :isRightFilled="'true'" />
-        <WheelsSVG :isRightFilled="'false'" />
+        <WheelsSVG :selectedPressureOption="selectedPressureOption" />
       </div>
 
-      <div class="control-panel__alerts animate__animated animate__fadeInDown animate__slow animate__delay-0.9s">
-        <v-card
-          class="mx-auto"
-          elevation="12"
-        >
+      <div
+        class="control-panel__alerts animate__animated animate__fadeInDown animate__slow animate__delay-0.9s"
+      >
+        <v-card class="mx-auto" elevation="12">
           <v-card-text>
             <div class="text">
-              <v-icon>mdi-map-marker-alert-outline</v-icon>Check your front right tire!
+              <v-icon>mdi-map-marker-alert-outline</v-icon>Check your front
+              right tire!
             </div>
           </v-card-text>
         </v-card>
       </div>
 
-      <div class="control-panel__switches  animate__animated animate__fadeInDown animate__slow animate__delay-0.9s">
+      <div
+        class="control-panel__switches  animate__animated animate__fadeInDown animate__slow animate__delay-0.9s"
+      >
         <div class="control-panel__switches--row">
           <div class="switch">
             <v-switch
@@ -55,7 +73,7 @@
               :label="switchLocked ? 'Locked' : 'Unlocked'"
               hide-details
             ></v-switch>
-            <v-icon class="left">mdi-lock-open</v-icon>          
+            <v-icon class="left">mdi-lock-open</v-icon>
             <v-icon class="right">mdi-lock</v-icon>
           </div>
 
@@ -65,9 +83,9 @@
               :label="switchAlarm ? 'Alarm On' : 'Alarm Off'"
               hide-details
             ></v-switch>
-            <v-icon class="left">mdi-alarm-off</v-icon>          
+            <v-icon class="left">mdi-alarm-off</v-icon>
             <v-icon class="right">mdi-alarm</v-icon>
-          </div> 
+          </div>
         </div>
         <div class="control-panel__switches--row">
           <div class="switch">
@@ -76,7 +94,7 @@
               :label="switchLights ? 'Lights On' : 'Lights Off'"
               hide-details
             ></v-switch>
-            <v-icon class="left">mdi-lightbulb-off</v-icon>          
+            <v-icon class="left">mdi-lightbulb-off</v-icon>
             <v-icon class="right">mdi-lightbulb-on</v-icon>
           </div>
 
@@ -86,166 +104,196 @@
               :label="switchHeating ? 'Heating On' : 'Heating Off'"
               hide-details
             ></v-switch>
-            <v-icon class="left">mdi-thermometer-low</v-icon>          
+            <v-icon class="left">mdi-thermometer-low</v-icon>
             <v-icon class="right">mdi-thermometer-high</v-icon>
-          </div> 
+          </div>
         </div>
       </div>
-    </main>   
+    </main>
   </div>
 </template>
 
 <script>
-    // external
-    import axios from 'axios';
+// external
+import axios from "axios";
 
-    // components
-    import WheelsSVG from "../components/WheelsSVG";
+// components
+import WheelsSVG from "../components/WheelsSVG";
 
-    export default {
-        name: "ControlPanel",
+// import dummy-data json file
+import DUMMY_DATA from "../assets/json/dummy-data.json";
 
-        components: {WheelsSVG},
+export default {
+  name: "ControlPanel",
 
-        data: () => ({
-            userData: [],
-            pressureOptions: [1.8,1.9,2.0,2.1],
-            switchLocked: true,
-            switchAlarm: false,
-            switchLights: false,
-            switchHeating: true
-        }),
+  components: { WheelsSVG },
 
-        created() {
-            this.getUserData()
-        },
+  dummyJsonDataWayOne: DUMMY_DATA,
 
-        methods: {
-            getUserData() {
-                axios({
-                  url: process.env.VUE_APP_API_URL + '/users',
-                  method: 'GET'
-                }).then((resp) => {
-                  console.log('--- Get Users using Lodash :: ', this.$_.get(resp, 'data'));
-                })
-            }
-        }
-    }
+  data: () => ({
+    userData: [],
+    pressureOptions: [
+      { position: "top-left", value: 1.0, animation: "animate__zoomInLeft" },
+      { position: "top-right", value: 2.0, animation: "animate__zoomInRight" },
+      { position: "bottom-left", value: 3.0, animation: "animate__zoomInLeft" },
+      {
+        position: "bottom-right",
+        value: 4.0,
+        animation: "animate__zoomInRight",
+      },
+    ],
+    selectedPressureOption: null,
+    switchLocked: true,
+    switchAlarm: false,
+    switchLights: false,
+    switchHeating: true,
+    dummyJsonDataWayTwo: DUMMY_DATA,
+  }),
+
+  created() {
+    this.getUserData();
+    this.getDummyDataFromJsonFile();
+    this.setDefaultPressureOption();
+  },
+
+  methods: {
+    getUserData() {
+      axios({
+        url: process.env.VUE_APP_API_URL + "/users",
+        method: "GET",
+      }).then((resp) => {
+        console.log(
+          "--- Get Users using Lodash :: ",
+          this.$_.get(resp, "data")
+        );
+      });
+    },
+    getDummyDataFromJsonFile() {
+      console.log(
+        "--- dummyJsonDataWayOne :: ",
+        this.$options.dummyJsonDataWayOne
+      );
+      console.log("--- dummyJsonDataWayTwo :: ", this.dummyJsonDataWayTwo);
+    },
+    setDefaultPressureOption() {
+      this.selectedPressureOption = this.pressureOptions[0];
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-  @import '../scss/main.scss';
+@import "../scss/main.scss";
 
-  .control-panel {
-    height: 100vh;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    
-    header{
-      color: $primary-color;
-    }
+.control-panel {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
 
-    &__select {
-      min-width: 400px;
-
-      .v-select__slot {
-        .v-label {
-          color: $primary-color;
-          font-weight: 500;
-        }
-      }
-    }    
-
-    &__wheels {
-      position: relative;
-      color: $gray;
-
-      .chassis {
-        transform: scale(1.5);
-        position: absolute;
-        top: 30%;
-        left: 45%;
-      }
-
-      .top-left {
-        position: absolute;
-        top: -10px;
-        left: 1%;
-      }
-
-      .top-right {
-        position: absolute;
-        right: 2%;
-        top: -6%;
-      }
-
-      .bottom-left {
-        position: absolute;
-        top: 44%;
-        left: 1%;
-      }
-
-      .bottom-right {
-        position: absolute;
-        right: 2%;
-        top: 44%;
-      }  
-    }
-
-    &__alerts {
-      min-width: 400px;
-      padding-top: 30px;
-
-      .v-card.v-sheet.theme--light {
-        background-color: $element;
-      }
-
-      .text {
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        color: $primary-color;
-        font-weight: 500;
-        font-size: $font-size-medium;
-
-        .v-icon {
-          margin-right: 10px;
-          color: $orange;
-        }
-      }
-    }
-
-    &__switches {
-      @extend %switch-theme;
-      padding: 30px 0;
-
-      &--row {
-        display: flex;
-        justify-content: space-evenly;
-        padding: 10px 0;
-      }
-
-      .switch {
-        position: relative;
-        width: 145px;
-
-        .v-icon {
-          position: absolute;
-          top: 30px;
-          color: $primary-color;
-          font-size: 20px;
-        }
-
-        .v-icon.left {
-          right: 32px;
-        }
-        .v-icon.right {
-          right: 0px;
-        }
-      }
-    }    
+  header {
+    color: $primary-color;
   }
 
+  &__select {
+    min-width: 400px;
+
+    .v-select__slot {
+      .v-label {
+        color: $primary-color;
+        font-weight: 500;
+      }
+    }
+  }
+
+  &__wheels {
+    position: relative;
+    color: $gray;
+
+    .chassis {
+      transform: scale(1.5);
+      position: absolute;
+      top: 30%;
+      left: 45%;
+    }
+
+    .top-left {
+      position: absolute;
+      top: -10px;
+      left: 1%;
+    }
+
+    .top-right {
+      position: absolute;
+      right: 2%;
+      top: -6%;
+    }
+
+    .bottom-left {
+      position: absolute;
+      top: 44%;
+      left: 1%;
+    }
+
+    .bottom-right {
+      position: absolute;
+      right: 2%;
+      top: 44%;
+    }
+  }
+
+  &__alerts {
+    min-width: 400px;
+    padding-top: 30px;
+
+    .v-card.v-sheet.theme--light {
+      background-color: $element;
+    }
+
+    .text {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      color: $primary-color;
+      font-weight: 500;
+      font-size: $font-size-medium;
+
+      .v-icon {
+        margin-right: 10px;
+        color: $orange;
+      }
+    }
+  }
+
+  &__switches {
+    @extend %switch-theme;
+    padding: 30px 0;
+
+    &--row {
+      display: flex;
+      justify-content: space-evenly;
+      padding: 10px 0;
+    }
+
+    .switch {
+      position: relative;
+      width: 145px;
+
+      .v-icon {
+        position: absolute;
+        top: 30px;
+        color: $primary-color;
+        font-size: 20px;
+      }
+
+      .v-icon.left {
+        right: 32px;
+      }
+      .v-icon.right {
+        right: 0px;
+      }
+    }
+  }
+}
 </style>
